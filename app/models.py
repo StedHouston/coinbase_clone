e16df2144f09bae09f6d25d458366bd1555192b3
StedHouston <s.houston406@gmail.com>
2020-09-08 18:06:00 -0500

    updated user model

--- app/models.py
+++ app/models.py
@@ -6,12 +6,16 @@ class User(db.Model):
   __tablename__ = 'users'
 
   id = db.Column(db.Integer, primary_key = True)
-  username = db.Column(db.String(40), nullable = False, unique = True)
+  first_name = db.Column(db.String(40), nullable = False)
+  last_name = db.Column(db.String(40), nullable = False)
   email = db.Column(db.String(255), nullable = False, unique = True)
+  account_balance = db.Column(db.String(255), nullable = False)
 
   def to_dict(self):
     return {
       "id": self.id,
-      "username": self.username,
-      "email": self.email
+      "first_name": self.first_name,
+      "last_name": self.last_name,
+      "email": self.email,
+      "account_balance": self.account_balance
     }
